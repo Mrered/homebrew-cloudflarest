7bffa51a4ff2a27b479cc5b1cfbdad4241b12d47
Mrered Cio <mrered@Mrered-MBA.local>
2023-04-08 21:35:00 +0800

    update arm64.rb

--- arm64.rb
+++ arm64.rb
@@ -1,10 +1,11 @@
-class CloudflarestArm64 < Formula
+class Arm64 < Formula
   desc "Select the best IP for your Mac on Cloudflare CDN"
   homepage "https://github.com/XIU2/CloudflareSpeedTest"
   url "https://github.com/Mrered/homebrew-cloudflarest/releases/download/v2.2.2/CloudflareST_darwin_arm64.tar.gz"
   sha256 "75e8996768dc5caff382bc8eeec64b6002675d82ba6816e0d8c75d9744761c28"
   version "2.2.2"
   license "GPL-3.0"
+  bottle :unneeded
 
   def install
     bin.install "CloudflareST"
@@ -14,5 +15,9 @@ class CloudflarestArm64 < Formula
     <<~EOS
     How to use: https://github.com/XIU2/CloudflareSpeedTest
     EOS
+  end
+
+  test do
+    system "#{bin}/CloudflareST"
   end
 end
